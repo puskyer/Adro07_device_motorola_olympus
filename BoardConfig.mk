dfadab344b300cb6481cba5c12d45de1a13c656d
fengyr168 <fengyr168@gmail.com>
2014-07-30 18:03:55 +0800

    cm 10.2 First Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,134 @@
+USE_CAMERA_STUB := true
+BOARD_USES_GENERIC_AUDIO := false
+
+# inherit from the proprietary version
+-include vendor/motorola/olympus/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := olympus
+TARGET_BOARD_PLATFORM := tegra
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := tegra2
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_FSTAB := device/motorola/olympus/root/fstab.olympus
+RECOVERY_FSTAB_VERSION := 2
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# Kernel configuration for inline building
+TARGET_KERNEL_CONFIG := tegra_olympus_defconfig
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_PAGE_SIZE := 0x00000800
+
+#storage
+TARGET_NO_BOOT := false
+TARGET_NO_RECOVERY := false
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_JANKY_BACKBUFFER := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8355840
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 407772160
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+BOARD_VOLD_MAX_PARTITIONS := 19
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+#UMS, MTP
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# WiFi
+BOARD_WLAN_DEVICE := bcmdhd
+ifeq ($(BOARD_WLAN_DEVICE),bcmdhd)
+BOARD_LEGACY_NL80211_STA_EVENTS:=true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/system/etc/nvram.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/vendor/firmware/fw_bcmdhd_apsta.bin nvram_path=/system/etc/nvram.txt"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
+endif
+ifeq ($(BOARD_WLAN_DEVICE),bcm4329)
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/vendor/firmware/fw_bcm4329.bin nvram_path=/system/etc/nvram.txt"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_SOCKET_IFACE    := eth0
+endif
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/motorola/olympus/config/bluetooth/vnd_olympus.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/olympus/config/bluetooth
+BOARD_BLUETOOTH_LIBBT_VNDCFG := device/motorola/olympus/config/bluetooth/bt_vendor.conf
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := true
+
+#EGL
+BOARD_EGL_CFG := device/motorola/olympus/config/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_USES_GL_VENDOR_EXTENSIONS := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+BOARD_USE_MOTO_DOCK_HACK := true
+BOARD_USES_AUDIO_LEGACY := true
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB -DMOTOROLA_UIDS -DICS_CAMERA_BLOB -DDISABLE_HW_ID_MATCH_CHECK
+BOARD_CAMERA_HAVE_ISO := true
+BOARD_MOBILEDATA_INTERFACE_NAME := "ppp0"
+TARGET_SCREEN_WIDTH:=540
+TARGET_SCREEN_HEIGHT:=960
+
+
+BOARD_SEPOLICY_DIRS += \
+        device/motorola/olympus/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    app.te \
+    device.te \
+    drmserver.te \
+    file.te \
+    genfs_contexts \
+    init.te \
+    media_app.te \
+    mediaserver.te \
+    platform_app.te \
+    radio.te \
+    release_app.te \
+    rild.te \
+    sensors_config.te \
+    shared_app.te \
+    surfaceflinger.te \
+    system_app.te \
+    system.te \
+    wpa_socket.te \
+    wpa.te \
+    zygote.te
---
+++ olympus.mk
@@ -0,0 +1,170 @@
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+LOCAL_PATH := device/motorola/olympus
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/root/init.target.rc:root/init.target.rc \
+    $(LOCAL_PATH)/root/init.target.usb.rc:root/init.target.usb.rc \
+    $(LOCAL_PATH)/root/ueventd.olympus.rc:root/ueventd.olympus.rc \
+    $(LOCAL_PATH)/root/fstab.olympus:root/fstab.olympus
+
+
+# helper scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scripts/pds_perm_fix.sh:system/bin/pds_perm_fix.sh \
+    $(LOCAL_PATH)/scripts/config_olympus_panel.sh:system/bin/config_olympus_panel.sh
+
+# sysctl conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/sysctl.conf:system/etc/sysctl.conf \
+    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf
+
+# Set en_US as default locale
+PRODUCT_LOCALES := en_US
+
+# olympus uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+# not exactly xhdpi, but we have enough RAM, why not use it?
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+$(call inherit-product-if-exists, vendor/motorola/olympus/olympus-vendor.mk)
+
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+
+#fs tools
+PRODUCT_PACKAGES += make_ext4fs \
+			e2fsck \
+ 			setup_fs
+
+#bluetooth
+PRODUCT_PACKAGES += l2ping \
+			hciconfig \
+			hcitool \
+			libnetcmdiface
+
+#Audio
+PRODUCT_PACKAGES += DockAudio \
+			audio.a2dp.default
+
+#Camera and lights
+PRODUCT_PACKAGES += Torch \
+			camerawrapper \
+			camera.tegra \
+			lights.olympus
+
+PRODUCT_PACKAGES += com.android.future.usb.accessory \
+			mot_boot_mode \
+			OlympusParts
+
+#wlan firmware
+PRODUCT_PACKAGES += \
+	olympus_fw_bcm4329.bin \
+	olympus_fw_bcm4329_apsta.bin \
+	olympus_fw_bcmdhd.bin \
+	olympus_fw_bcmdhd_apsta.bin \
+	olympus_fw_bcmdhd_p2p.bin
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/scripts/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
+    $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/config/touchpad.cfg:system/etc/touchpad/22/touchpad.cfg \
+    $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/config/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+#keyboard files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl \
+    $(LOCAL_PATH)/keychars/tegra-kbc.kcm.bin:system/usr/keychars/tegra-kbc.kcm.bin \
+    $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/BTC_USB_Cordless_Mouse.kl \
+    $(LOCAL_PATH)/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    $(LOCAL_PATH)/keylayout/qtouch-obp-ts.kl:system/usr/keylayout/qtouch-obp-ts.kl \
+    $(LOCAL_PATH)/config/qtouch-obp-ts.idc:system/usr/idc/qtouch-obp-ts.idc \
+    $(LOCAL_PATH)/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl:system/usr/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl \
+    $(LOCAL_PATH)/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+    $(LOCAL_PATH)/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
+    $(LOCAL_PATH)/keychars/evfwd.kcm.bin:system/usr/keychars/evfwd.kcm.bin \
+    $(LOCAL_PATH)/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
+    $(LOCAL_PATH)/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/usb_keyboard_102_en_us.kcm.bin \
+    $(LOCAL_PATH)/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/Motorola_Bluetooth_Wireless_Keyboard.kl \
+    $(LOCAL_PATH)/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/Motorola_Bluetooth_Wireless_Keyboard.kcm.bin
+
+# Permission files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+#RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+	rild.libpath=/system/lib/libmoto_ril.so \
+	persist.ril.mux.noofchannels=9 \
+	persist.ril.mux.ttydevice=/dev/ttyHS3 \
+	ro.telephony.call_ring.multiple=false \
+	ro.telephony.call_ring.delay=500 \
+	persist.ril.modem.ttydevice=/dev/ttySPI0 \
+	persist.ril.modem.mode=2 \
+	persist.ril.features=0x2020 \
+	persist.ril.mux.retries=500 \
+	persist.ril.mux.sleep=2 \
+	ro.kernel.android.ril=yes \
+	persist.ril.pppd.start.fail.max=16 \
+	mobiledata.interfaces=ppp0 \
+	ro.telephony.ril.v3=signalstrength,skipbrokendatacall,writeaidonly
+
+# Default network type.
+# 0 -> WCDMA preferred
+# 3 -> GSM Auto(PRL)
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.default_network=3
+
+#wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.supplicant_scan_interval=30 \
+	wifi.interface=wlan0
+
+#bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.bt.bdaddr_path=/pds/bt/bt_bdaddr
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072 \
+	persist.sys.ui.hw=true \
+	ro.sf.lcd_density=240
+
+# Forbid format of these partitions in mount menu :
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.cwm.forbid_format=/misc,/radio,/pds,/bootloader,/recovery,/efs
+
+# Default USB Mode
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mass_storage
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.authentec.fingerprint.jar=/system/framework/am2app.jar
+	ro.authentec.fingerprint.so=/system/lib/libam2app.so
+
+# 1% battery
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.product.use_charge_counter=1
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_NAME := generic_olympus
+PRODUCT_DEVICE := olympus
+PRODUCT_MODEL := MB860
