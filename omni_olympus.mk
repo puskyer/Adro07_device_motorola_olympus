58176502cf215f85432a43cc2c69138c8021dae7
fengyr168 <fengyr168@gmail.com>
2014-07-30 18:24:15 +0800

    omni_olympus.mk changes to sync with Omni

--- omni_olympus.mk
+++ omni_olympus.mk
@@ -1,23 +1,43 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-## Release name
-PRODUCT_RELEASE_NAME := Atrix
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+
 
 #use low quality videos
 $(call inherit-product, frameworks/base/data/videos/VideoPackage1.mk)
 
-## Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
  
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
 ## Inherit device configuration
 $(call inherit-product, device/motorola/olympus/olympus.mk)
 
-PRODUCT_NAME := cm_olympus
+## This root
+$(call inherit-product, vendor/supersu/supersu.mk)
+
  
 ## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := Atrix
+PRODUCT_NAME := omni_olympus
 PRODUCT_DEVICE := olympus
-
 PRODUCT_MANUFACTURER=motorola
 
 ## Device fingerprint
